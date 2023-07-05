956584c495bb8eea016ecb39c7b28a4d0291c5a4
Mirai-Principal <46869971+Mirai-Principal@users.noreply.github.com>
2023-07-05 08:36:19 -0500

    Add files via upload

---
+++ threadexample/src/main/java/net/osgg/threadexample/App.java
@@ -0,0 +1,15 @@
+package net.osgg.threadexample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	new ColourThread("Black").start();
+        new ColourThread("White").start();
+        new ColourThread("green").start();
+    }
+}
